9c12bf2a85097eb9896605682e402b004bcee789
Nawsen <wannesvandorpe@hotmail.com>
2015-10-17 22:58:11 +0200

    Basic mockup detail view

---
+++ Project/Views/TripDetailView.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace Project.Views
+{
+    public partial class TripDetailView : PhoneApplicationPage
+    {
+        public TripDetailView()
+        {
+            InitializeComponent();
+            itemList.Items.Add(new TripListItemView());
+            itemList.Items.Add(new TripListItemView());
+            itemList.Items.Add(new TripListItemView());
+
+        }
+    }
+}
---
+++ Project/Views/TripListItemView.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace Project.Views
+{
+    public partial class TripListItemView : UserControl
+    {
+        public TripListItemView()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox_Checked(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void checkBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
